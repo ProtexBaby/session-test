827277f395d0397d503e1e69914f237052d2ae62
ProtexBaby <protextbaby@gmail.com>
2025-04-03 01:02:08 +0700

    Update

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function startServer() {
 		}
 
 		sessionPort += 1
-        Sessions.set(sessionPort, `EscapeServer-${sessionPort}`)
+        Sessions.set(sessionPort.toString(), `EscapeServer-${sessionPort}`)
 
 		pm2.start({
         	name: `EscapeServer-${sessionPort}`,
